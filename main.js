b1a8007906d673caf04ce059bdf05895970e3eb9
arbaaz <mirzaarbaaz123@gmail.com>
2022-06-21 23:13:24 -0400

    coding practice

--- main.js
+++ main.js
@@ -512,5 +512,14 @@ Examples (Input -> Output)
 let usdcny = (usd) => `${(usd*6.75).toFixed(2)} Chinese Yuan`
 
 /*
+Write a simple regex to validate a username. Allowed characters are:
 
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
 */
+
+function validateUsr(username) {
+    return /^[0-9a-z_]{4,16}$/.test(username)
+}
