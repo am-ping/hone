9c6765edbaaf4896e09c67fab557b0b3b85a4aa5
arbaaz <mirzaarbaaz123@gmail.com>
2022-07-18 23:27:04 -0400

    coding practice

--- main.js
+++ main.js
@@ -122,8 +122,15 @@ let squareSum = (nums) => nums.reduce((a,b)=> a + (b ** 2),0)
 
 
 /*
-
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+Return True if you're better, else False!
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
 */
+
+function betterThanAverage(classPoints, yourPoints) {
+    return yourPoints > classPoints.reduce((a,b)=> a+b,0) / classPoints.length
+}
 
 
 /*
