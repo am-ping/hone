c7d540ab040edf86043c4377faf63e3ce64e5f5a
arbaaz <mirzaarbaaz123@gmail.com>
2022-07-20 23:38:32 -0400

    coding practice

--- main.js
+++ main.js
@@ -1244,3 +1244,20 @@ const horizonDistance = (h, r) => {
     let c = h + r //hypotenuse
     return Math.sqrt(c**2 - b**2) //side 2
 }
+
+
+/*
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+Example: (input --> output)
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+*/
+
+function DNAStrand(dna){
+  return dna.split("").map(item => {
+    return (item === 'A') ? item = "T"
+    : (item === 'T') ? item = "A"
+    : (item === 'G') ? item = "C"
+    : item = "G"
+  }).join("")
+}
