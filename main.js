f0d00c78ecc2f0ee33251c1a50e8c98aa777bd31
arbaaz <mirzaarbaaz123@gmail.com>
2022-08-01 23:41:20 -0400

    coding practice

--- main.js
+++ main.js
@@ -1447,3 +1447,33 @@ function wave(str){
     return item
   }).filter(item => item !== item.toLowerCase())
 }
+
+
+/*
+In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
+*/
+
+/* Recursion solution:
+function factorial(n) {
+  if (n < 0 || n > 12)
+    throw new RangeError();
+  return n <= 1 ? 1 : n * factorial(n - 1);
+}
+*/
+
+function factorial(n){
+  let arr = []
+  if (n < 0 || n > 12) {
+    throw new RangeError('')
+  }  
+  for (let i = 1; i <= n; i++) {
+    arr.push(i)
+  }
+  return arr.reduce((a,b) => a * b, 1)
+}
+
+
+/*
+
+*/
