841ee99996f8e6ab34369f0ebc348fe1dc1ea9ec
arbaaz <mirzaarbaaz123@gmail.com>
2022-08-01 21:30:02 -0400

    coding practice

--- main.js
+++ main.js
@@ -1432,5 +1432,18 @@ function countPositivesSumNegatives(input) {
 
 
 /*
-
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+Rules
+ 1.  The input string will always be lower case but maybe empty.
+ 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+Example
+wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
 */
+
+function wave(str){
+  return str.split("").map((item,i) => {
+    item = str
+    item = item.split("").map((a,b) => (b === i) ? item[i] = item[i].toUpperCase() : a).join('')
+    return item
+  }).filter(item => item !== item.toLowerCase())
+}
