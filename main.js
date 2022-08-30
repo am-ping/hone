34e19b1be1363c7feb86720a9d82e8c9c55e9382
arbaaz <mirzaarbaaz123@gmail.com>
2022-08-29 23:33:14 -0400

    coding practice

--- main.js
+++ main.js
@@ -3435,3 +3435,10 @@ six_toast(12) == 6
 */
 
 let sixToast = (num) => Math.abs(num - 6)
+
+
+/*
+You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
+*/
+
+let differenceInAges = (ages) => [Math.min(...ages), Math.max(...ages), Math.max(...ages) - Math.min(...ages)]
