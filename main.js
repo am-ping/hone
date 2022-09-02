baeb911b7fc1edb2c10ddb2d4b04a4e619c83655
arbaaz <mirzaarbaaz123@gmail.com>
2022-09-02 12:08:11 -0400

    coding practice

--- main.js
+++ main.js
@@ -3609,3 +3609,29 @@ function adjacentElementsProduct(arr) {
   arr.pop()
   return Math.max(...arr)
 }
+
+
+/*
+Your task, is to create NxN multiplication table, of size provided in parameter.
+for example, when given size is 3:
+1 2 3
+2 4 6
+3 6 9
+for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
+*/
+
+function multiplicationTable(size) {
+  let result = []
+  for (let i = 0; i < size; i++) {
+    result[i] = []
+    for(let j = 0; j < size; j++) {
+      result[i][j] = (i + 1) * (j + 1)
+    }
+  }
+  return result
+}
+
+
+/*
+
+*/
