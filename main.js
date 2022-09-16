4ab633e4945601b6fd6e8ee14a78cca089f8d5ee
arbaaz <mirzaarbaaz123@gmail.com>
2022-09-16 11:27:49 -0400

    coding practice

--- main.js
+++ main.js
@@ -4725,5 +4725,15 @@ function containAllRots(str, arr) {
 
 
 /*
-
+Implement a function which accepts 2 arguments: string and separator.
+The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+For example:
+splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
 */
+
+function splitAndMerge(str, separator) {
+  return str.split(' ').map(item => item.split('').join(separator)).join(' ')
+}
