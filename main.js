90d552062b394779e5a55ebc0e47c2a9cd091fe5
arbaaz <mirzaarbaaz123@gmail.com>
2022-09-18 19:20:45 -0400

    coding practice

--- main.js
+++ main.js
@@ -4744,3 +4744,21 @@ Your task is to return a number from a string. You will be given a string of num
 */
 
 let FilterString = str => +str.replace(/\D/g,'')
+
+
+/*
+The goal is to create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times).
+Examples
+numberToPower(3,2)  // -> 9 ( = 3 * 3 )
+numberToPower(2,3)  // -> 8 ( = 2 * 2 * 2 )
+numberToPower(10,6) // -> 1000000
+Note: Math.pow and some other Math functions like eval() and ** are disabled.
+*/
+
+let numberToPower = (n, p) => {
+  let arr = []
+  for (let i = 1; i <= p; i++) {
+    arr.push(n)
+  }
+  return arr.reduce((a,b) => a * b, 1)
+}
