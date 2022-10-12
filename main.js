2e6a9cde243ae060301f9ddf536937c03df91fcf
arbaaz <mirzaarbaaz123@gmail.com>
2022-10-12 00:10:53 -0400

    coding practice

--- main.js
+++ main.js
@@ -5194,3 +5194,10 @@ function sumCubes(n){
   }
   return count
 }
+
+
+/*
+Your code will show Full name of the neighbor and the truncated version of the name as an array. If the number of the characters in name is less than or equal to two, it will return an array containing only the name as is"
+*/
+
+let whoIsPaying = name => name.length <= 2 ? [name] : [name, name.slice(0,2)]
