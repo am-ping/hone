a7db416ac318a58a4f59c12a5498a1e666a469dc
arbaaz <mirzaarbaaz123@gmail.com>
2022-10-14 11:34:20 -0400

    coding practice

--- main.js
+++ main.js
@@ -5216,3 +5216,14 @@ function squares(x, n) {
   }
   return arr
 }
+
+
+/*
+Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+duck_duck_goose([a, b, c, d], 1) should return a.name
+duck_duck_goose([a, b, c, d], 5) should return a.name
+duck_duck_goose([a, b, c, d], 4) should return d.name
+*/
+
+let duckDuckGoose = (p, g) => p[(g - 1) % p.length].name
+
