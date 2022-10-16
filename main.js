30a80f00a4ba5b013d0db8cfff6f038b4d3bce09
arbaaz <mirzaarbaaz123@gmail.com>
2022-10-16 02:16:20 -0400

    coding practice

--- main.js
+++ main.js
@@ -5240,3 +5240,22 @@ specialNumber(59) ==> return "NOT!!"
 */
 
 let specialNumber = n => /[6-9]/.test(n) ? "NOT!!" : "Special!!"
+
+
+/*
+Your task is to create a function that, given a proper first and last name, will return the correct alias.
+Notes:
+Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given.
+If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.'
+*/
+
+function aliasGen(f, l){
+  f = f.toUpperCase()[0]
+  l = l.toUpperCase()[0]
+  return (/\d/.test(f) || /\d/.test(l)) ? "Your name must start with a letter from A - Z." : `${firstName[f]} ${surname[l]}`
+}
