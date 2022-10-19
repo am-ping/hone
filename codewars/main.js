fffd39be9ef7fa44c6bde92af01860ceccd5e569
arbaaz <mirzaarbaaz123@gmail.com>
2022-10-19 10:35:43 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -5293,3 +5293,13 @@ function countDeafRats(town) {
         .reduce((a,b) => a + b, 0)
 }
 
+
+/*
+Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+*/
+
+class Cat extends Animal {
+  speak() {
+    return `${this.name} meows.`
+  }
+}
