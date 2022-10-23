41dadeb2340d3babea0cffe337df8b990e40dd0c
arbaaz <mirzaarbaaz123@gmail.com>
2022-10-23 00:32:30 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -5332,3 +5332,13 @@ Determine the total number of digits in the integer (n>=0) given as input to the
 
 let digits = n => String(n).length
 
+
+/*
+Due to another of his misbehaved, the primary school's teacher of the young Gauß, Herr J.G. Büttner, to keep the bored and unruly young schoolboy Karl Friedrich Gauss busy for a good long time, while he teaching arithmetic to his mates, assigned him the problem of adding up all the whole numbers from 1 through a given number n.
+Your task is to help the young Carl Friedrich to solve this problem as quickly as you can; so, he can astonish his teacher and rescue his recreation interval.
+f(n=100) // returns 5050 
+It's your duty to verify that n is a valid positive integer number. If not, please, return false
+*/
+
+let f = n => (parseInt(n) === n && n > 0) ? n * (n + 1)/2 : false
+
