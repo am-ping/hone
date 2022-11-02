a209ce6c76c2d186da065d2711e067274572fd35
arbaaz <mirzaarbaaz123@gmail.com>
2022-11-02 10:48:00 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -5461,3 +5461,15 @@ Create a method none? (JS none) that accepts an array and a block (JS: a functio
 
 let none = (arr, func) => !arr.some(func)
 
+
+/*
+Create a function called one that accepts two params: sequence and function
+and returns true only if the function in the params returns true for exactly one (1) item in the sequence.
+one([1, 3, 5, 6, 99, 1, 3], bigger_than_ten) -> true
+one([1, 3, 5, 6, 99, 88, 3], bigger_than_ten) -> false
+one([1, 3, 5, 6, 5, 1, 3], bigger_than_ten) -> false
+*/
+
+let one = (arr, func) => arr.filter(func).length === 1
+
+
