cc1f182105533545e8271a80121c8e01d5939922
arbaaz <mirzaarbaaz123@gmail.com>
2022-11-06 11:02:17 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -5518,3 +5518,14 @@ function encode(str, n) {
   return Array.from(str, (c, i) => c.charCodeAt(0) - 96 + +key[i % key.length])
 }
 
+
+/*
+Create a function that finds the integral of the expression passed. In order to find the integral all you need to do is add one to the exponent (the second argument), and divide the coefficient (the first argument) by that new number.
+For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent, and divided the coefficient by that new number).
+ 3, 2  -->  "1x^3"
+12, 5  -->  "2x^6"
+20, 1  -->  "10x^2"
+*/
+
+let integrate = (c, e) => `${c/(e + 1)}x^${e + 1}`
+
