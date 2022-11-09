9517897dd760212bef3751c38934246e3fad7f93
arbaaz <mirzaarbaaz123@gmail.com>
2022-11-09 14:46:21 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -5571,3 +5571,16 @@ function toLeetSpeak(s) {
   s.replace(/[ABCEGHILOSTZ]/g, l => a[l])
 }
 
+
+/*
+Create a function that takes an integer argument of seconds and converts the value into a string describing how many hours and minutes comprise that many seconds. Any remaining seconds left over are ignored.
+3600 --> "1 hour(s) and 0 minute(s)"
+3601 --> "1 hour(s) and 0 minute(s)"
+3500 --> "0 hour(s) and 58 minute(s)"
+323500 --> "89 hour(s) and 51 minute(s)"
+*/
+
+function toTime(s) {
+  return `${Math.floor(s / 3600)} hour(s) and ${Math.floor(s % 3600 / 60)} minute(s)`
+}
+
