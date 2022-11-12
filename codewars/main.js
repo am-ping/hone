0f8cf28549fbe6878244038f5021885e33763394
arbaaz <mirzaarbaaz123@gmail.com>
2022-11-12 16:00:38 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -5610,3 +5610,27 @@ In this kata, the function will take a string as its argument, and return a stri
 
 let toFreud = s => s.replace(/[^ ]+/g,'sex')
 
+
+/*
+Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+With list [20,37,20,21] and number 1, the result would be [20,37,21].
+*/
+
+/* alternative solution
+const deleteNth = (a, x) => {
+  let m = {};
+  return a.filter( v => ( m[v] = m[v]+1||1 ) <= x );
+}
+*/
+
+function deleteNth(arr, n){
+  let ans = []
+  for (let i = 0; i < arr.length; i++) {
+    if (ans.filter(item => item === arr[i]).length < n ) {
+      ans.push(arr[i])
+    }
+  }
+  return ans
+}
+
