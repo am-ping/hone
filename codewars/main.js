d3c04c11ccec50b068b942959b7d36b540fd3df4
arbaaz <mirzaarbaaz123@gmail.com>
2022-11-14 11:04:16 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -5646,3 +5646,23 @@ Examples (input -> output):
 Array.prototype.remove_ = function(integer_list, values_list){
   return integer_list.filter(item => !values_list.includes(item))
 }
+
+
+/*
+Find the sum of all multiples of n below m
+n and m are natural numbers (positive integers)
+m is excluded from the multiples
+sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+sumMul(4, -7)  ==> "INVALID"
+*/
+
+function sumMul(n, m){
+  if (n >= m) return "INVALID"
+  let sum = 0
+  for (let i = n; i < m; i += n) {
+    sum += i
+  }
+  return sum
+}
