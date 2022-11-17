d1ad788aafd2ec5facf39d29c374a1ee076e5462
arbaaz <mirzaarbaaz123@gmail.com>
2022-11-17 02:27:42 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -5712,3 +5712,13 @@ function quote(fighter) {
   : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
 }
 
+
+/*
+This kata is about converting numbers to their binary or hexadecimal representation:
+If a number is even, convert it to binary.
+If a number is odd, convert it to hex.
+Numbers will be positive. The hexadecimal string should be lowercased.
+*/
+
+let evensAndOdds = num => (num % 2) ? num.toString(16) : num.toString(2)
+
