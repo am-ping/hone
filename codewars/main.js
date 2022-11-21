d698bd9451e0b668031f5f4396677e678fbc21dd
arbaaz <mirzaarbaaz123@gmail.com>
2022-11-21 11:07:40 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -5783,3 +5783,17 @@ function sortMyString(s) {
   return even + ' ' + odd
 }
 
+
+/*
+You know that a piece of paper has a thickness of 0.0001m. Given distance in units of meters, calculate how many times you have to fold the paper to make the paper reach this distance.
+(If you're not familiar with the concept of folding a paper: Each fold doubles its total thickness.)Note: Of course you can't do half a fold. You should know what this means ;P
+Also, if somebody is giving you a negative distance, return null
+*/
+
+function foldTo(distance) {
+  let count = 0
+  for (let i = 0.0001; i <= distance; i += i) {
+    count++
+  }
+  return distance < 0 ? null : count
+}
