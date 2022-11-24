3b408501c6bb9fcae694d2a03c7281e4086192cf
arbaaz <mirzaarbaaz123@gmail.com>
2022-11-24 11:05:45 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -5821,3 +5821,13 @@ You will be given an array of numbers which can be used using the String.fromCha
 */
 
 let ArrowFunc = arr => arr.map(item => String.fromCharCode(item)).join('')
+
+
+/*
+Wilson primes satisfy the following condition. Let P represent a prime number. Then,
+((P-1)! + 1) / (P * P)
+should give a whole number.
+Your task is to create a function that returns true if the given number is a Wilson prime.
+*/
+
+let amIWilson = p => p == 5 || p == 13 || p == 563
