eaefb8964da371bea6260c64cccdd87cf2603217
arbaaz <mirzaarbaaz123@gmail.com>
2022-12-03 18:26:01 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -5956,3 +5956,14 @@ function perfectSquare(str){
   str = str.split('\n')
   return str.every(item => item.length == str.length && !/[^.]/.test(item))
 }
+
+
+
+/*
+Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+explode("102269")
+should return :
+"12222666666999999999"
+*/
+
+let explode = s => s.replace(/\d/g, (item) => item.repeat(+item))
