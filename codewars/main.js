083cd0e7ed524f5169c74db8b417704ca5927faf
arbaaz <mirzaarbaaz123@gmail.com>
2022-12-06 11:22:59 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -5990,3 +5990,15 @@ function divisibleByThree(str){
   str = [...str].forEach(item => sum += +item)
   return sum % 3 == 0
 }
+
+
+/*
+My friend wants a new band name for her band. She like bands that use the formula: "The" + a noun with the first letter capitalized, for example:
+"dolphin" -> "The Dolphin"
+However, when a noun STARTS and ENDS with the same letter, she likes to repeat the noun twice and connect them together with the first and last letter, combined into one word (WITHOUT "The" in front), like this:
+"alaska" -> "Alaskalaska"
+*/
+
+function bandNameGenerator(str) {
+  return str[0] == str[str.length - 1] ? str[0].toUpperCase() + str.slice(1) + str.slice(1) : `The ${str[0].toUpperCase() + str.slice(1)}`
+}
