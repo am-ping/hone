ad883824d4e393dea9a8ffd505ff043b57af07af
arbaaz <mirzaarbaaz123@gmail.com>
2022-12-09 11:19:40 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6040,3 +6040,15 @@ async function promiseHelloWorld() {
   return 'Hello World!'
 }
 
+
+/*
+Your function should return True or False based on the following criteria.
+You should take an umbrella if it's currently raining or if it's cloudy and the chance of rain is over 0.20.
+You shouldn't take an umbrella if it's sunny unless it's more likely to rain than not.
+The options for the current weather are sunny, cloudy, and rainy.
+For example, take_umbrella('sunny', 0.40) should return False.
+*/
+
+function takeUmbrella(weather, chance) {
+  return weather == 'rainy' || (weather == 'cloudy' && chance > 0.2 ) || chance > 0.5
+}
