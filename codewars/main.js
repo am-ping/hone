16f3ebbfca60b81525013d58438ae0bc50cd9574
arbaaz <mirzaarbaaz123@gmail.com>
2022-12-14 17:09:58 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6124,3 +6124,19 @@ function pairs(array, count = 0) {
       count += 1
   return count
 }
+
+
+/*
+Codewars Bar recommends you drink 1 glass of water per standard drink so you're not hungover tomorrow morning.
+Your fellow coders have bought you several drinks tonight in the form of a string. Return a string suggesting how many glasses of water you should drink to not be hungover.
+Examples
+"1 beer"  -->  "1 glass of water"
+because you drank one standard drink
+"1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"  -->  "10 glasses of water"
+because you drank ten standard drinks
+*/
+
+function hydrate(s) {
+  s = [...s.replace(/[^\d]/g, '')].reduce((a,b) => a + +b, 0)
+  return `${s} glass${s > 1 ? 'es' : ''} of water`
+}
