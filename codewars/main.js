97b90abe316ff087299fe48baa6bd64ddd27abd4
arbaaz <mirzaarbaaz123@gmail.com>
2022-12-17 16:27:26 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6185,3 +6185,18 @@ class Block {
     return 2 * (l * w + l * h + w * h)
   }
 }
+
+
+/*
+Please return the gap between the first position of c and the last position of c.
+If there are a lot of c in the str, should return a positive integer; If there is only one c in str, should return 0; If there is no c in the str, should return -1. Retrieval should not ignored the case.
+firstToLast("ababc","a") should return 2(2-0)
+firstToLast("ababc","c") should return 0(4-4)
+firstToLast("ababc","d") should return -1
+*/
+
+function firstToLast(str,c){
+  let f = str.indexOf(c)
+  let l = str.lastIndexOf(c)
+  return f == -1 ? -1 : l - f
+}
