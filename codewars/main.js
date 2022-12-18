78e5965137192951b0001552211cd1f0292dcb68
arbaaz <mirzaarbaaz123@gmail.com>
2022-12-18 17:23:27 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6200,3 +6200,16 @@ function firstToLast(str,c){
   let l = str.lastIndexOf(c)
   return f == -1 ? -1 : l - f
 }
+
+
+/*
+Complete the solution so that it takes the object (JavaScript/CoffeeScript) or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
+*/
+
+function solution(pairs){
+  return Object.keys(pairs)
+    .map(item => item + ' = ' + pairs[item] )
+    .join(',')
+}
