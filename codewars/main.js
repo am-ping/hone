fb3cf0d1a830791594b625155b60c7e233def5a6
arbaaz <mirzaarbaaz123@gmail.com>
2022-12-19 11:06:20 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6213,3 +6213,23 @@ function solution(pairs){
     .map(item => item + ' = ' + pairs[item] )
     .join(',')
 }
+
+
+/*
+You are given a string of letters and an array of numbers.
+The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
+After each removal the size of the string decreases (there is no empty space).
+Return the only letter left.
+
+Example:
+
+let str = "zbk", arr = [0, 1]
+    str = "bk", arr = [1]
+    str = "b", arr = []
+    return 'b'
+*/
+
+function lastSurvivor([...letters], coords) {
+  coords.forEach( item => letters = letters.filter((l,i) => i !== item) )
+  return letters.join('')
+}
