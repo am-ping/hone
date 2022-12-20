9cf837db2f6c872d98822a8711735be4afda3a31
arbaaz <mirzaarbaaz123@gmail.com>
2022-12-20 18:53:21 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6233,3 +6233,23 @@ function lastSurvivor([...letters], coords) {
   coords.forEach( item => letters = letters.filter((l,i) => i !== item) )
   return letters.join('')
 }
+
+
+/*
+You have access to the ship "draft" and "crew". "Draft" is the total ship weight and "crew" is the number of humans on the ship.
+Each crew member adds 1.5 units to the ship draft. If after removing the weight of the crew, the draft is still more than 20, then the ship is worth looting. Any ship weighing that much must have a lot of booty!
+Add the method
+isWorthIt
+to decide if the ship is worthy to loot. For example:
+titanic.isWorthIt() // return false
+*/
+
+class Ship {
+  constructor(draft,crew) {
+   this.draft = draft;
+   this.crew = crew;
+  }
+  isWorthIt() {
+    return this.draft - 1.5 * this.crew > 20;
+  }
+}
