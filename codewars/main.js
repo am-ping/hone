680a0456b34255c5947138ad0d9324f8c96e038a
arbaaz <mirzaarbaaz123@gmail.com>
2022-12-22 11:00:10 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6262,3 +6262,20 @@ Create a function that returns the average of an array of numbers ("scores"), ro
 function average(scores) {
   return Math.round(scores.reduce((a,b) => a + b, 0) / scores.length)
 }
+
+
+/*
+Given an array with exactly 5 strings "a", "b" or "c" (chars in Java, characters in Fortran), check if the array contains three and two of the same values.
+["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
+["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
+["a", "a", "a", "a", "a"] ==> false // 5x "a"
+*/
+
+function checkThreeAndTwo(arr) {
+  let a = arr.filter(item => item == 'a').length
+  let b = arr.filter(item => item == 'b').length
+  let c = arr.filter(item => item == 'c').length
+  return (a === 3 || b === 3 || c === 3)  
+          && (a === 2 || b === 2 || c === 2)
+}
+
