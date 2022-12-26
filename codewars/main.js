de35f9ee7d06e6b95e560ac6f2cac52e6e6c3d2b
arbaaz <mirzaarbaaz123@gmail.com>
2022-12-26 12:54:34 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6314,3 +6314,16 @@ function calculate(str) {
                     : count += +item)
   return count.toString()
 }
+
+
+/*
+Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+("hello", "world", "l")       ==>  "held"
+("coding", "anywhere", "n")   ==>  "codinywhere"
+("jason", "samson", "s")      ==>  "jasamson"
+("wonderful", "people", "e")  ==>  "wondeople"
+*/
+
+function stringMerge(s1, s2, l){
+  return s1.slice(0, s1.indexOf(l)) + s2.slice(s2.indexOf(l))
+}
