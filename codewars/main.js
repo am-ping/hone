ac381a9e9d067b715af4828cb02334c1f651f627
arbaaz <mirzaarbaaz123@gmail.com>
2022-12-30 11:16:38 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6373,3 +6373,13 @@ function shorter_reverse_longer(a,b){
   let l = s == a ? b : a
   return s + [...l].reverse().join('') + s 
 }
+
+
+/*
+Write a function filterLucky/filter_lucky() that accepts a list of integers and filters the list to only include the elements that contain the digit 7.
+ghci> filterLucky [1,2,3,4,5,6,7,68,69,70,15,17]
+[7,70,17]
+Don't worry about bad input, you will always receive a finite list of integers.
+*/
+
+let filterLucky = x => x.filter(num => /7/.test(num))
