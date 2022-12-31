10e5667bebca0420c2a49bccc6b85e1be22f112e
arbaaz <mirzaarbaaz123@gmail.com>
2022-12-31 18:13:08 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6383,3 +6383,17 @@ Don't worry about bad input, you will always receive a finite list of integers.
 */
 
 let filterLucky = x => x.filter(num => /7/.test(num))
+
+
+/*
+Write a function that returns the number of occurrences of an element in an array.
+var arr = [0, 1, 2, 2, 3];
+arr.numberOfOccurrences(0) === 1;
+arr.numberOfOccurrences(4) === 0;
+arr.numberOfOccurrences(2) === 2;
+arr.numberOfOccurrences("a") === 0;
+*/
+
+Array.prototype.numberOfOccurrences = function(n) {
+  return this.filter(item => item == n).length
+}
