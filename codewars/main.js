8cf5024bc6bed3ce1b60cdea5afb96f9018cb654
arbaaz <mirzaarbaaz123@gmail.com>
2023-01-08 18:18:07 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6506,3 +6506,25 @@ function to_nato(str) {
             .map(item => item in NATO ? item = NATO[item] : item)
             .join(' ')
 }
+
+
+/*
+Create a function that returns the CSV representation of a two-dimensional numeric array. Array's length > 2.
+input:
+   [[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]] 
+output:
+     '0,1,2,3,4\n'
+    +'10,11,12,13,14\n'
+    +'20,21,22,23,24\n'
+    +'30,31,32,33,34'
+*/
+/* alternative solution
+let toCsvText = arr => arr.join('\n')
+*/
+
+function toCsvText(arr) {
+  return arr.map((item,i) => i !== arr.length - 1 ? item.join(',') + '\n' : item).join('')
+}
