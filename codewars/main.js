2c01491b1821619f3420179a91e937f6b87b3ad0
AM-png <63935001+AM-png@users.noreply.github.com>
2023-01-07 19:12:55 -0500

    Update main.js

--- codewars/main.js
+++ codewars/main.js
@@ -1,4 +1,18 @@
 /*
+In this kata, we will make a function to test whether a period is late.
+Our function will take three parameters:
+last - The Date object with the date of the last period
+today - The Date object with the date of the check
+cycleLength - Integer representing the length of the cycle in days
+Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.
+*/
+
+function periodIsLate(last, today, cycleLength) {
+  return (today-last) / 86400000 > cycleLength
+}
+
+
+/*
 Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 */
 
