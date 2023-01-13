50fec391ca207c3d3ae571d7f1dca5728297418f
arbaaz <mirzaarbaaz123@gmail.com>
2023-01-13 11:03:20 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6578,3 +6578,12 @@ function mostFrequentItemCount(arr) {
   }
   return count
 }
+
+
+/*
+Create a function called args_count that returns the number of arguments provided
+args_count(1, 2, 3) -> 3
+args_count(1, 2, 3, 10) -> 4
+*/
+
+let args_count = (...count) => count.length
