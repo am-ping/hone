414340af723d501edb189079cddd1157ed2c2796
arbaaz <mirzaarbaaz123@gmail.com>
2023-01-24 10:43:10 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6768,3 +6768,14 @@ Write a function insert_dash(num) / insertDash(num) / InsertDash(int num) that w
 */
 
 let insertDash = num => num.toString().replace(/[13579](?=[13579])/g, "$&-")
+
+
+/*
+You have a quiver of arrows, but some have been damaged. The quiver contains arrows with an optional range information (different types of targets are positioned at different ranges), so each item is an arrow.
+You need to verify that you have some good ones left, in order to prepare for battle:
+anyArrows([{range: 5}, {range: 10, damaged: true}, {damaged: true}])
+If an arrow in the quiver does not have a damaged status, it means it's new.
+The expected result is a boolean, indicating whether you have any good arrows left.
+*/
+
+let anyArrows = arrows => arrows.some(item => !item.damaged)
