5263789f106d8d26468e19f13485984e7940c568
arbaaz <mirzaarbaaz123@gmail.com>
2023-01-28 12:12:05 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6848,3 +6848,17 @@ guessBlue() should return the probability of drawing a blue marble, expressed as
 function guessBlue(blueS, redS, blueP, redP) {
   return (blueS - blueP)/(redS - redP + blueS - blueP)
 }
+
+
+/*
+In your class, you have started lessons about arithmetic progression. Since you are also a programmer, you have decided to write a function that will return the first n elements of the sequence with the given common difference d and first element a. Note that the difference may be zero!
+The result should be a string of numbers, separated by comma and space.
+# first element: 1, difference: 2, how many: 5
+arithmetic_sequence_elements(1, 2, 5) == "1, 3, 5, 7, 9"
+*/
+
+function arithmeticSequenceElements(a, d, n) {
+  let arr = [a]
+  while (--n) arr.push(a += d)
+  return arr.join(', ')
+}
