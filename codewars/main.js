bf6f10787bc0fd7b28a50e4fc3aff40285ab4b7d
arbaaz <mirzaarbaaz123@gmail.com>
2023-01-29 00:35:19 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6862,3 +6862,23 @@ function arithmeticSequenceElements(a, d, n) {
   while (--n) arr.push(a += d)
   return arr.join(', ')
 }
+
+
+/*
+Complete the function that takes two numbers as input, num and nth and return the nth digit of num (counting from right to left).
+If num is negative, ignore its sign and treat it as a positive value
+If nth is not positive, return -1
+Keep in mind that 42 = 00042. This means that findDigit(42, 5) would return 0
+5673, 4 --> 5
+129, 2 --> 2
+-2825, 3 --> 8
+-456, 4 --> 0
+0, 20 --> 0
+65, 0 --> -1
+*/
+
+function findDigit(num, i) {
+  if (i < 1) return -1
+  let n = [...(String(num))].reverse().filter(item => /\d/.test(item))
+  return !n[i - 1] ? 0 : +n[i - 1]
+}
