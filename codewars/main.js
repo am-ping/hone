4de96033f0d435b1820d93c69aff88138e368377
arbaaz <mirzaarbaaz123@gmail.com>
2023-01-30 11:02:19 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6882,3 +6882,13 @@ function findDigit(num, i) {
   let n = [...(String(num))].reverse().filter(item => /\d/.test(item))
   return !n[i - 1] ? 0 : +n[i - 1]
 }
+
+
+/*
+Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+*/
+
+function switcher(x) {
+  let a = ' ?!abcdefghijklmnopqrstuvwxyz'.split('').reverse()
+  return x.map(item => a[+item - 1]).join('')
+}
