b60c63bd1665f0a5f5dcb1662613ad2272d46f0d
arbaaz <mirzaarbaaz123@gmail.com>
2023-02-04 17:26:35 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6974,3 +6974,20 @@ function nbYear(p0, percent, aug, p) {
   }
   return years
 }
+
+
+/*
+Some people just have a first name; some people have first and last names and some people have first, middle and last names.
+You task is to initialize the middle names (if there is any).
+'Jack Ryan'                   => 'Jack Ryan'
+'Lois Mary Lane'              => 'Lois M. Lane'
+'Dimitri'                     => 'Dimitri'
+'Alice Betty Catherine Davis' => 'Alice B. C. Davis'
+*/
+/*
+const initializeNames = name => name.replace(/ (\w)\w*(?= )/g, ' $1.')
+*/
+
+function initializeNames(name){
+  return name.split(' ').map((item,i,arr) => i > 0 && i !== arr.length - 1 ? `${item[0]}.` : item).join(' ')
+}
