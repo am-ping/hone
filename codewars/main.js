9d2a7e7618ef396d1709c0fee21d48253574fb1c
arbaaz <mirzaarbaaz123@gmail.com>
2023-02-05 20:06:17 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -6991,3 +6991,18 @@ const initializeNames = name => name.replace(/ (\w)\w*(?= )/g, ' $1.')
 function initializeNames(name){
   return name.split(' ').map((item,i,arr) => i > 0 && i !== arr.length - 1 ? `${item[0]}.` : item).join(' ')
 }
+
+
+/*
+Complete the method which accepts an array of integers, and returns one of the following:
+"yes, ascending" - if the numbers in the array are sorted in an ascending order
+"yes, descending" - if the numbers in the array are sorted in a descending order
+"no" - otherwise
+You can assume the array will always be valid, and there will always be one correct answer.
+*/
+
+function isSortedAndHow(arr) {
+  return arr.every((x,i) => i==0 || arr[i] >= arr[i-1]) ? 'yes, ascending'
+       : arr.every((x,i) => i==0 || arr[i] <= arr[i-1]) ? 'yes, descending'
+       : 'no'
+}
