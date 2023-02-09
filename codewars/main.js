66e8a3b04280ec1fed60d20406b8dcf273151148
arbaaz <mirzaarbaaz123@gmail.com>
2023-02-09 10:18:07 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7063,3 +7063,15 @@ function wordValue(a) {
   let c = a.map(item => [...item.replace(/ /g, '')].reduce((a,b) => a + b.charCodeAt() - 96, 0))
   return c.map((item,i) => item * (i + 1))
 }
+
+
+/*
+We want to create a constructor function 'NameMe', which takes first name and last name as parameters. The function combines the first and last names and saves the value in "name" property.
+We already implemented that function, but when we actually run the code, the "name" property is accessible, but the "firstName" and "lastName" is not accessible. All the properties should be accessible. Can you find what's wrong with it? A test fixture is also available
+*/
+
+function NameMe(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.name = first + ' ' + last
+}
