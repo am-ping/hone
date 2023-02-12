5fde0ae0f35f9535b87214333ea6dc2a0af6a1e5
arbaaz <mirzaarbaaz123@gmail.com>
2023-02-11 21:32:02 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7097,3 +7097,34 @@ Write a function named isNice/IsNice that returns true if its array argument is 
 function isNice(arr){
   return !!arr.length && arr.every(item => arr.includes(item + 1) || arr.includes(item - 1))
 }
+
+
+/*
+Extra perfect number is the number that first and last bits are set bits.
+
+Task
+Given a positive integer N , Return the extra perfect numbers in range from 1 to N
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+Number passed is always Positive .
+
+Returned array/list should contain the extra perfect numbers in ascending order from lowest to highest
+
+Input >> Output Examples
+extraPerfect(3)  ==>  return {1,3}
+Explanation:
+(1)10 =(1)2
+First and last bits as set bits.
+
+(3)10 = (11)2
+First and last bits as set bits.
+*/
+
+function extraPerfect(n){
+  let arr = []
+  for (let i = 1; i <= n; i += 2) {
+    arr.push(i)
+  }
+  return arr
+}
