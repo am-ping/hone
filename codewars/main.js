dd2f51374a6b9df5faded5d9aec646dd65d2542e
arbaaz <mirzaarbaaz123@gmail.com>
2023-02-12 07:31:58 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7128,3 +7128,20 @@ function extraPerfect(n){
   }
   return arr
 }
+
+
+/*
+Coding in function colorOf. function accept 3 parameter:r g b. It means value of color red green and blue. the value range is 0-255.
+Use toString(16) Convert numbers r g b to hex string form. at last, combine them to a web color code and return it.
+the color code should starting with "#". and then use 2 characters per color.
+colorOf(255,0,0) should return "#ff0000"
+colorOf(0,111,0) should return "#006f00"
+colorOf(1, 2 ,3) should return "#010203"
+*/
+
+function colorOf(r,g,b){
+  r = r.toString(16).length == 1 ? '0' + r.toString(16) : r.toString(16)
+  g = g.toString(16).length == 1 ? '0' + g.toString(16) : g.toString(16)
+  b = b.toString(16).length == 1 ? '0' + b.toString(16) : b.toString(16)
+  return '#' + r + g + b
+}
