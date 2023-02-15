8c4c230d662610cbcfbe421a0659e54317995753
arbaaz <mirzaarbaaz123@gmail.com>
2023-02-14 19:47:49 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7155,3 +7155,12 @@ greet(null) === null; // Return null if input is null
 */
 
 let greet = name => name ? 'hello ' + name + '!' : null
+
+
+/*
+Complete the code which should return true if the given object is a single ASCII letter (lower or upper case), false otherwise.
+*/
+
+String.prototype.isLetter = function() {
+  return /^[a-z]$/i.test(this)
+}
