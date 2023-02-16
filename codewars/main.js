6ea6d8dd1ccadab2cdc81a25d4ffbac096b89117
arbaaz <mirzaarbaaz123@gmail.com>
2023-02-15 19:53:55 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7164,3 +7164,13 @@ Complete the code which should return true if the given object is a single ASCII
 String.prototype.isLetter = function() {
   return /^[a-z]$/i.test(this)
 }
+
+
+/*
+Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+flatten([1,2,3]) // => [1,2,3]
+flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+flatten([[[1,2,3]]]) // => [[1,2,3]]
+*/
+
+let flatten = arr => [].concat(...arr)
