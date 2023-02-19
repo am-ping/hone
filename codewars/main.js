0ad745a382440a831219755ae4fe17ef0017365c
arbaaz <mirzaarbaaz123@gmail.com>
2023-02-19 01:27:52 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7205,3 +7205,22 @@ Even though there are no spaces, it is still valid because none are needed:
 */
 
 let validSpacing = s => s.trim() == s && !s.includes("  ")
+
+
+/*
+You already implemented a Cube class, but now we need your help again! I'm talking about constructors. We don't have one. Let's code two: One taking an integer and one handling no given arguments!
+Also we got a problem with negative values. Correct the code so negative values will be switched to positive ones!
+The constructor taking no arguments should assign 0 to Cube's Side property.
+*/
+
+class Cube {
+  constructor(side = 0) {
+    this.side = side
+  }
+  getSide() {
+    return Math.abs(this.side)
+  }
+  setSide(side) {
+    this.side = side
+  }
+}
