d53f1de7cfba68d9abe2574cb10c11d7b154e184
arbaaz <mirzaarbaaz123@gmail.com>
2023-02-20 02:18:27 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7224,3 +7224,17 @@ class Cube {
     this.side = side
   }
 }
+
+
+/*
+Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
+Examples:
+[0, 5, 1, 3, 2, 9, 7, 6, 4] --> 8
+[9, 2, 4, 5, 7, 0, 8, 6, 1] --> 3
+*/
+
+function getMissingElement(arr){
+  for (let i = 0; i < 10; i++) {
+    if (!arr.includes(i)) return i 
+  }
+}
