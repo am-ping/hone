7acad4ca64473dd6c519ae416bcc9231e7ce46fd
arbaaz <mirzaarbaaz123@gmail.com>
2023-02-23 09:04:17 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7310,3 +7310,20 @@ function fiveLine(s){
   let t = s.trim()
   return `${t}\n${t.repeat(2)}\n${t.repeat(3)}\n${t.repeat(4)}\n${t.repeat(5)}`
 }
+
+
+/*
+Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
+Array/list size is at least 2 .
+Array/list's numbers Will be only Positives
+Repetition of numbers in the array/list could occur.
+Input >> Output Examples
+productArray ({12,20}) ==>  return {20,12}
+Explanation:
+The first element in prod [] array 20 is the product of all array's elements except the first element
+The second element 12 is the product of all array's elements except the second element
+*/
+
+function productArray(numbers){
+  return numbers.map(x => numbers.reduce((a,b) => a * b) / x)
+}
