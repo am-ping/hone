deafd0c8d7401424359d6245582a22aed7a73ab9
arbaaz <mirzaarbaaz123@gmail.com>
2023-02-24 13:55:08 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7327,3 +7327,23 @@ The second element 12 is the product of all array's elements except the second e
 function productArray(numbers){
   return numbers.map(x => numbers.reduce((a,b) => a * b) / x)
 }
+
+
+/*
+Given the following input array:
+var list1 = [
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
+];
+write a function that when executed as findAdmin(list1, 'JavaScript') returns only the JavaScript developers who are GitHub admins:
+[
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' }
+]
+*/
+
+function findAdmin(list, lang) {
+  return list.filter(item => item.language == lang && item.githubAdmin == 'yes')
+}
