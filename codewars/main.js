5e6aba9ca5ec7db56f7288e8044d156e61e3bd61
arbaaz <mirzaarbaaz123@gmail.com>
2023-03-01 14:40:47 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7405,3 +7405,22 @@ Dog.prototype.bark = function() {
 }
 let snoopy = new Dog("Beagle");
 let scoobydoo = new Dog("Great Dane");
+
+
+/*
+Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+*/
+
+function validParentheses(parens) {
+  let n = 0
+  for (let i = 0; i < parens.length; i++) {
+    if (parens[i] == '(') n++
+    if (parens[i] == ')') n--
+    if (n < 0) return false
+  }
+  return n == 0
+}
