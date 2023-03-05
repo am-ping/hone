d0d6a578ba4fcb7f97fd9ff5883852c27261b529
arbaaz <mirzaarbaaz123@gmail.com>
2023-03-05 02:07:28 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7463,3 +7463,26 @@ The input will always be an array.
 function deepCount(a){
   return a.reduce((acc, item) => acc + (Array.isArray(item) ? deepCount(item) : 0), a.length)
 }
+
+
+/*
+Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+Based on: http://oj.leetcode.com/problems/two-sum/
+twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0]
+*/
+
+function twoSum(num, target) {
+  let z
+  let o
+  num.forEach((item,i) => {
+    if ( num.includes(target - item) ) {
+      z = i
+      o = num.indexOf( target - item )
+    }
+  })
+  return [z, o]
+}
+
+
