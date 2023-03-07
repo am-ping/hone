6af545e587218aee9d18962974d836c1218b0d0a
arbaaz <mirzaarbaaz123@gmail.com>
2023-03-07 13:08:38 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7496,3 +7496,15 @@ isPowerOfTwo(333)  // -> false
 */
 
 let isPowerOfTwo = n => Math.log2(n) % 1 === 0
+
+
+/*
+Complete the function to create backronyms. Transform the given string (without spaces) to a backronym, using the preloaded dictionary and return a string of words, separated with a single space (but no trailing spaces).
+The keys of the preloaded dictionary are uppercase letters A-Z and the values are predetermined words, for example: dict["P"] == "perfect"
+"dgm" ==> "disturbing gregarious mustache"
+"lkj" ==> "literal klingon joke"
+*/
+
+function makeBackronym (str){
+  return str.toUpperCase().split('').map(item => dict[item])
+}
