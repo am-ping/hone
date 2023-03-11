23ff911a97b85b7fa6a95b94e53a436b298e37e1
arbaaz <mirzaarbaaz123@gmail.com>
2023-03-11 00:03:34 -0500

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7541,3 +7541,25 @@ Example 1: Input: {1, 2}, {3, 4}, 15 --> Output: 5
 function lostSheep(friday,saturday,total){
   return friday.concat(saturday).reduce((a,b) => a - b, total)
 }
+
+
+/*
+Coding in function whatNumberIsIt. function accept 1 parameter:n. it's a number.
+To judge the number n. If n is one of the above five constants, return one of these string:
+"Input number is Number.MAX_VALUE"
+"Input number is Number.MIN_VALUE"
+"Input number is Number.NaN"
+"Input number is Number.NEGATIVE_INFINITY"
+"Input number is Number.POSITIVE_INFINITY"
+Other values should return "Input number is xxx". xxx means this number.
+*/
+
+function whatNumberIsIt(n){
+  let str = n == Infinity ? "Number.POSITIVE_INFINITY"
+  : n == -Infinity ? 'Number.NEGATIVE_INFINITY'
+  : Number.isNaN(n) ? 'Number.NaN'
+  : n == 1.7976931348623157e+308 ? 'Number.MAX_VALUE'
+  : n == 5e-324 ? 'Number.MIN_VALUE'
+  : n
+  return `Input number is ${str}`
+}
