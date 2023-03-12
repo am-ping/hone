ae2fb3061b94cd794a54f3fd4980cb671f0a9766
arbaaz <mirzaarbaaz123@gmail.com>
2023-03-12 03:54:04 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7563,3 +7563,17 @@ function whatNumberIsIt(n){
   : n
   return `Input number is ${str}`
 }
+
+
+/*
+you will be given a string of four words. Your job is to turn them in to Gordon language.
+Rules:
+Obviously the words should be Caps, Every word should end with '!!!!', Any letter 'a' or 'A' should become '@', Any other vowel should become '*'.
+*/
+
+function gordon(ramsay) {
+  return ramsay
+          .toUpperCase()
+          .replace(/\w+/g, '$&!!!!')
+          .replace(/[AEIOU]/g, v => v == 'A' ? '@' : '*')
+}
