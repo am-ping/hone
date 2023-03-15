11ae8454c0eca0f54705538d8643ecc19eee6281
arbaaz <mirzaarbaaz123@gmail.com>
2023-03-15 02:11:21 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7593,3 +7593,17 @@ There is an object/class already created called MrFreeze. Mark this object as fr
 */
 
 Object.freeze(MrFreeze)
+
+
+/*
+Each floating-point number should be formatted that only the first two decimal places are returned. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+Don't round the numbers! Just cut them after two decimal places!
+Right examples:  
+32.8493 is 32.84  
+14.3286 is 14.32
+Incorrect examples (e.g. if you round the numbers):  
+32.8493 is 32.85  
+14.3286 is 14.33
+*/
+
+const twoDecimalPlaces = num => Math.trunc(num * 100) / 100
