ce227665989b1947b5922e6bf6dc59f502b9b1af
arbaaz <mirzaarbaaz123@gmail.com>
2023-03-21 00:05:28 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7727,3 +7727,16 @@ function getOrder(input) {
   + str.replace(/(Coke)|./gs, '$1').replace(/(Coke)/g, 'Coke ')
   return list.slice(0, str.length - 1)
 }
+
+
+/*
+While developing a website, you detect that some of the members have troubles logging in. Searching through the code you find that all logins ending with a "_" make problems. So you want to write a function that takes an array of pairs of login-names and e-mails, and outputs an array of all login-name, e-mails-pairs from the login-names that end with "_".
+If you have the input-array:
+[ [ "foo", "foo@foo.com" ], [ "bar_", "bar@bar.com" ] ]
+it should output
+[ [ "bar_", "bar@bar.com" ] ]
+*/
+
+function searchNames( logins ){
+  return logins.filter(item => item[0].slice(-1) === '_')
+}
