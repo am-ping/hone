87c775a25c201f8df128825b89f090dfa9312ff8
arbaaz <mirzaarbaaz123@gmail.com>
2023-03-31 00:48:33 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -7938,3 +7938,15 @@ function eachCons(arr, n) {
   }
 	return rarr.filter(item => item.length == n)
 }
+
+
+/*
+The objective is to set each of the 26 characters of the output string to either '1' or '0' based on the fact whether the Nth letter of the alphabet is present in the input (independent of its case).
+So if an 'a' or an 'A' appears aanywhere in the input string (any number of times), set the first character of the output string to '1', otherwise to '0'. if 'b' or 'B' appears in the string, set the second character to '1', and so on for the rest of the alphabet.
+"a   **&  cZ"  =>  "10100000000000000000000001"
+*/
+
+function change(str){
+  let s = 'abcdefghijklmnopqrstuvwxyz'
+  return s.replace(/\w/g, item => str.toLowerCase().includes(item) ? 1 : 0)
+}
