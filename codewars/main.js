b6c38284b9aa23d4a5a4303836812251483f6aca
arbaaz <mirzaarbaaz123@gmail.com>
2023-04-03 08:58:31 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8011,3 +8011,14 @@ Your task is to create a method, Math.roundTo(number, precision) which rounds a 
 */
 
 Math.roundTo = (number, precision) => +number.toFixed(precision)
+
+
+/*
+You are given a program sumSquares that takes an array as input and returns the sum of the squares of each item in an array. For example:
+sumSquares([1,2,3,4,5]) === 55 // 1 ** 2 + 2 ** 2 + 3 ** 2 + 4 ** 2 + 5 ** 2
+sumSquares([7,3,9,6,5]) === 200
+sumSquares([11,13,15,18,2]) === 843
+Shorten the code such that it meets the requirements.
+*/
+
+let sumSquares = arr => arr.reduce((a,b) => a + b ** 2, 0)
