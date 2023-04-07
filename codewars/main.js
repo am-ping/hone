7d9527dd22ef946c5885bb09f4e73131c776df66
arbaaz <mirzaarbaaz123@gmail.com>
2023-04-07 01:44:32 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8069,3 +8069,13 @@ class Person {
     return `Welcome to Planet Earth ${raceName}`
   }
 }
+
+
+/*
+Write function heron which calculates the area of a triangle with sides a, b, and c (x, y, z in COBOL).
+*/
+
+function heron(a, b, c) {
+  let s = ( a + b + c ) / 2
+  return Math.sqrt( s * (s - a) * (s - b) * (s - c) )
+}
