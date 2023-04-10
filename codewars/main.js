b99145efb1a812e4c19a75333586a44ac118b2b8
arbaaz <mirzaarbaaz123@gmail.com>
2023-04-10 00:50:23 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8133,3 +8133,16 @@ let plus = num => (sec_operand) => sec_operand + num
 let minus = num => (sec_operand) => sec_operand - num
 let times = num => (sec_operand) => sec_operand * num
 let dividedBy = num => (sec_operand) => Math.floor(sec_operand / num)
+
+
+/*
+The Stanton measure of an array is computed as follows: count the number of occurences for value 1 in the array. Let this count be n. The Stanton measure is the number of times that n appears in the array.
+Write a function which takes an integer array and returns its Stanton measure.
+The Stanton measure of [1, 4, 3, 2, 1, 2, 3, 2] is 3, because 1 occurs 2 times in the array and 2 occurs 3 times.
+The Stanton measure of [1, 4, 1, 2, 11, 2, 3, 1] is 1, because 1 occurs 3 times in the array and 3 occurs 1 time.
+*/
+
+function stantonMeasure(arr) {
+  let count = n => arr.filter(item => item === n).length
+  return count(count(1))
+}
