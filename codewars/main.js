1e6988f6103ffad71fdac62a56254b65bbc591ef
arbaaz <mirzaarbaaz123@gmail.com>
2023-04-12 14:30:08 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8155,3 +8155,12 @@ unscrambleEggs("Beggegeggineggneggeregg"); => "Beginner"
 */
 
 let unscrambleEggs = str => str.replace(/egg/g, '')
+
+
+/*
+AddExtra method adds a new item to the list and returns the list. The new item can be any object, and it does not matter. (lets say you add an integer value, like 13)
+In our test case we check to assure that the returned list has one more item than the input list. However the method needs some modification to pass this test.
+P.S. You have to create a new list and add a new item to that.
+*/
+
+let addExtra = numArr => [...numArr, 1]
