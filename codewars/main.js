8c1482ffdfe8c51c0db0157668ed90eccdde8373
arbaaz <mirzaarbaaz123@gmail.com>
2023-04-15 08:10:56 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8185,3 +8185,19 @@ function FindFunction(func, arr) {
   let f = func.find(item => item instanceof Object === true)
   return arr.filter(f)
 }
+
+
+/*
+Write a single function that can be invoked by either
+```javascript
+sum(2,3); //5 //or sum(2)(3); //5```
+clojure (sum 2 3) ;; 5 ;;or ((sum 2) 3) ;; 5
+Both of these examples should return the sum of the 2 numbers.
+*/
+
+function sum(a,b) {
+  if (arguments.length === 1) {
+    return function(b) {return a + b}
+  }
+  return a + b
+}
