6ca7cf69be1b22b5dc54b20e8661ac200be90177
arbaaz <mirzaarbaaz123@gmail.com>
2023-04-17 00:53:43 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8213,3 +8213,13 @@ zebulansNightmare('main') == 'main'
 */
 
 let zebulansNightmare = funcName => funcName.replace(/_./g, c => c[1].toUpperCase())
+
+
+/*
+Create a function that returns the total of a meal including tip and tax. You should not tip on the tax.
+You will be given the subtotal, the tax as a percentage and the tip as a percentage. Please round your result to two decimal places.
+*/
+
+function calculate_total(subtotal, tax, tip) {
+  return +(subtotal * (1 + tip / 100 + tax / 100)).toFixed(2)
+}
