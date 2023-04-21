633ce9f691dc670b818abe32ae64b81b8df6c4f1
arbaaz <mirzaarbaaz123@gmail.com>
2023-04-21 03:28:21 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8265,3 +8265,11 @@ function reverseIt(data){
   : typeof data === 'number' ? +[...data.toString()].reverse().join('')
   : data
 }
+
+
+/*
+Implement a function to calculate the sum of the numerical values in a nested list. For example :
+sumNested([1, [2, [3, [4]]]]) => 10
+*/
+
+let sumNested = arr => arr.flat(Infinity).reduce((a,b) => a + b, 0)
