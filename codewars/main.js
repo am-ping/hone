da495e2b7ba565479926ee07eba15c2e352f76cb
arbaaz <mirzaarbaaz123@gmail.com>
2023-04-30 02:46:58 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8395,3 +8395,23 @@ Write a function that returns true if the input number is -0 and false otherwise
 */
 
 let isNegativeZero = n => Object.is(n,-0)
+
+
+/*
+JavaScript provides an Array.prototype.forEach method that allows you to iterate over array values. For this exercise you will create your own array method called 'each'. It will be similar to the forEach method, except for one difference. If the callback function returns true then the loop will stop and no additional values will be iterated.
+
+The following shows a contrived example of how this new method would be used:
+
+var letters = ['a', 'b', 'c', 'd', 'e']
+var allowedLetters = []
+letters.each(function(letter, index){
+  // break out of the loop if we reached a letter with the value 'd'
+  if(letter == 'd') {
+    return true;
+  }
+  allowedLetters.push(letter);   
+})
+// allowedLetters should equal ['a', 'b', 'c']
+*/
+
+Array.prototype.each = [].some
