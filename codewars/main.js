61492d0f1c7bf8ee7b1edfaf2f9d2077b56804e5
arbaaz <mirzaarbaaz123@gmail.com>
2023-05-02 14:48:38 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8427,3 +8427,14 @@ calculate(2,4)(3,7,1) // should return 17
 */
 
 let calculate = (...a) => (...b) => [...a, ...b].reduce((acc, cur) => acc + cur, 0)
+
+
+/*
+We will be given the sale price (discounted price), and the sale percentage, our job is to figure out the original price.
+Given an item at $75 sale price after applying a 25% discount, the function should return the original price of that item before applying the sale percentage, which is ($100.00) of course, rounded to two decimal places.
+DiscoverOriginalPrice(75, 25) => 100.00M where 75 is the sale price (discounted price), 25 is the sale percentage and 100 is the original price
+*/
+
+function discoverOriginalPrice(dPrice, sPercentage){
+  return +( ( dPrice / (100 - sPercentage) ) * 100 ).toFixed(2)
+}
