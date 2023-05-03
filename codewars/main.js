c69d2bb94e86e33e1b9e72619b1778e06c5f49cd
arbaaz <mirzaarbaaz123@gmail.com>
2023-05-03 15:02:12 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8438,3 +8438,14 @@ DiscoverOriginalPrice(75, 25) => 100.00M where 75 is the sale price (discounted 
 function discoverOriginalPrice(dPrice, sPercentage){
   return +( ( dPrice / (100 - sPercentage) ) * 100 ).toFixed(2)
 }
+
+
+/*
+Check if it is a vowel(a, e, i, o, u,) on the n position in a string (the first argument). Don't forget about uppercase.
+checkVowel('cat', 1)  ->   true // 'a' is a vowel
+checkVowel('cat', 0)  ->   false // 'c' is not a vowel
+checkVowel('cat', 4)  ->   false // this position doesn't exist
+P.S. If n < 0, return false
+*/
+
+let checkVowel = (s,i) => 'aeiouAEIOU'.includes(s[i])
