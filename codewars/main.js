db6a75e56fe18237adaff724be2046ad2df9438f
arbaaz <mirzaarbaaz123@gmail.com>
2023-05-05 09:59:14 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8468,3 +8468,15 @@ class Solution{
     console.log("Hello World!")
   }
 }
+
+
+/*
+Your task is to write an update for a lottery machine. Its current version produces a sequence of random letters and integers (passed as a string to the function). Your code must filter out all letters and return unique integers as a string, in their order of first appearance. If there are no integers in the string return "One more run!"
+"hPrBKWDH8yc6Lt5NQZWQ"  -->  "865"
+"ynMAisVpHEqpqHBqTrwH"  -->  "One more run!"
+"555"                   -->  "5"
+*/
+
+function lottery(str){
+  return (/[0-9]/.test(str)) ? [...new Set( str.replace(/[^0-9]/g, '') )].join('') : "One more run!"
+}
