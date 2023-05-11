021586e6720512ad802fce3b7e9bc4bc04eb965c
arbaaz <mirzaarbaaz123@gmail.com>
2023-05-11 12:37:47 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8567,3 +8567,17 @@ dollar: 1.00
 */
 
 let changeCount = c => `$${c.split(' ').reduce((a,b)=> a + CHANGE[b], 0).toFixed(2)}`
+
+
+/*
+Your task is to extend JavaScript String object with a .contains(substr) method, so you can check if the string contains a substring.
+By default, it has to be a case-insensitive check. But you also have to provide a second optional bool parameter, so the method would be able to do a case-sensitive check as well.
+'Hello, World!'.contains('Hel');         // true
+'Hello, World!'.contains('wor');         // true
+'Hello, World!'.contains('wor', true);   // false (case-sensitive check)
+'Hello, World!'.contains('a');           // false
+*/
+
+String.prototype.contains = function(sub, sens) {
+  return sens ? this.includes(sub) : this.toLowerCase().includes(sub.toLowerCase())
+}
