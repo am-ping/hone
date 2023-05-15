cd19c4a6caacfe3b097a21c879ae40b8b2abcfdc
arbaaz <mirzaarbaaz123@gmail.com>
2023-05-15 09:16:35 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8652,3 +8652,22 @@ function createGrid(m, n, position) {
   }
   return res.join('\n')
 }
+
+
+/*
+There are two lists, possibly of different lengths. The first one consists of keys, the second one consists of values. Write a function createDict(keys, values) that returns a dictionary created from keys and values. If there are not enough values, the rest of keys should have a None (JS null)value. If there not enough keys, just ignore the rest of values.
+
+keys = ['a', 'b', 'c', 'd']
+values = [1, 2, 3]
+createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3, 'd': null}
+
+keys = ['a', 'b', 'c']
+values = [1, 2, 3, 4]
+createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3}
+*/
+
+function createDict(keys, values) {
+  return keys.reduce((o, v, i) => {
+    return o[v] = i in values ? values[i] : null, o
+  }, {})
+}
