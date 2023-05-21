88e98e8df00c0badbd6a08dad3840145794a5805
arbaaz <mirzaarbaaz123@gmail.com>
2023-05-21 17:11:13 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8840,3 +8840,12 @@ function findMissingNumber(str) {
   arr.forEach((item,i) => n == item - 1 ? n = item : n)
   return arr[arr.length - 1] == n ? 0 : n + 1
 }
+
+
+/*
+The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+Given a string of binary, return the version the photocopier gives you as a string.
+*/
+
+let broken = x => x.replace(/[10]/g, item => item == 1 ? 0 : 1)
