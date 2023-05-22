f8542d6d79204d03a514178e9844ea263de4af6c
arbaaz <mirzaarbaaz123@gmail.com>
2023-05-22 09:10:27 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8849,3 +8849,25 @@ Given a string of binary, return the version the photocopier gives you as a stri
 */
 
 let broken = x => x.replace(/[10]/g, item => item == 1 ? 0 : 1)
+
+
+/*
+Terminal game turn function
+You are creating a text-based terminal version of your favorite board game. In the board game, each turn has six steps that must happen in this order: roll the dice, move, combat, get coins, buy more health, and print status.
+You are using a library (Game.Logic in C#) that already has the functions below. Create a function named doTurn/DoTurn/do_turn that calls the functions in the proper order as described in the paragraph above.
+- combat
+- buyHealth
+- getCoins
+- printStatus
+- rollDice
+- move
+*/
+
+function doTurn () {
+  rollDice()
+  move()
+  combat()
+  getCoins()
+  buyHealth()
+  printStatus()
+}
