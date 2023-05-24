2898a4bd41530ff5369488597d518ee0901b67de
arbaaz <mirzaarbaaz123@gmail.com>
2023-05-23 20:16:51 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8871,3 +8871,16 @@ function doTurn () {
   buyHealth()
   printStatus()
 }
+
+
+/*
+This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.
+
+Examples:
+
+[[1],[2]] => true
+['1','2'] => false
+[{1:1},{2:2}] => false
+*/
+
+const arrCheck = value => value.every(item => Array.isArray(item))
