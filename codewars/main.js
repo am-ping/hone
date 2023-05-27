3215288123616361f6387794e8258881a38533df
arbaaz <mirzaarbaaz123@gmail.com>
2023-05-27 19:36:44 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8916,3 +8916,14 @@ Write reverseList function that simply reverses lists.
 */
 
 let reverseList = arr => arr.reverse()
+
+
+/*
+Write a function that returns a sequence (index begins with 1) of all the even characters from a string. If the string is smaller than two characters or longer than 100 characters, the function should return "invalid string".
+"abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
+"a"             --> "invalid string"
+*/
+
+function evenChars(str) {
+  return str.length < 2 || str.length > 100 ? "invalid string" : str.split('').filter((item,i) => i % 2)
+}
