374745956114845a6ef9f5976b00ce2f979421db
arbaaz <mirzaarbaaz123@gmail.com>
2023-05-31 19:08:49 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -8981,3 +8981,22 @@ function wordSearch(query, seq){
   let arr = seq.filter(item => item.toLowerCase().includes(query.toLowerCase()))
   return arr.length == 0 ? ["Empty"] : arr
 }
+
+
+/*
+Given an array, find the duplicates in that array, and return a new array of those duplicates. The elements of the returned array should appear in the order when they first appeared as duplicates.
+
+Note: numbers and their corresponding string representations should not be treated as duplicates (i.e., "1" != 1).
+
+[1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]  ==>  [4, 3, 1]
+[0, 1, 2, 3, 4, 5]                ==>  []
+*/
+
+function duplicates(arr) {
+  let a = [], b = []
+  arr.forEach(item => {
+    if (a.includes(item)) b.push(item)
+    a.push(item)
+  })
+  return [...new Set(b)]
+}
