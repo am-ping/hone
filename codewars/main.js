7a88351998c604000b3c8c651f0dce7e11d8246e
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-01 18:37:59 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9000,3 +9000,16 @@ function duplicates(arr) {
   })
   return [...new Set(b)]
 }
+
+
+/*
+In this kata, you will be given an array. Each value represents a meeting room. Your job? Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
+
+'X' --> busy
+'O' --> empty
+If all rooms are busy, return "None available!"
+*/
+
+function meeting(x){
+  return x.indexOf('O') == -1 ? "None available!" : x.indexOf('O')
+}
