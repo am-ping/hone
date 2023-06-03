2dc1fd91fa0394f40fa9f9a01d0b531474170494
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-03 16:17:36 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9041,3 +9041,20 @@ function orderFood(l) {
   l.forEach(item => res[item["meal"]] ? res[item["meal"]]++ : res[item["meal"]] = 1 )
   return res
 }
+
+
+/*
+You will be given a string representing the garden such as:
+
+garden = 'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock gravel gravel gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel gravel gravel gravel gravel snail gravel gravel rock gravel snail slug gravel gravel spider gravel gravel gravel gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel'
+Rake out any items that are not a rock or gravel and replace them with gravel such that:
+
+garden = 'slug spider rock gravel gravel gravel gravel gravel gravel gravel'
+Returns a string with all items except a rock or gravel replaced with gravel:
+
+garden = 'gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'
+*/
+
+function rakeGarden(garden) {
+  return garden.split(' ').map(item => item !== 'gravel' && item !== "rock" ? item = 'gravel' : item).join(' ')
+}
