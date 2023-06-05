5ac9228aeecb01f195e7f5cc0184936cac6665af
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-04 21:19:59 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9058,3 +9058,14 @@ garden = 'gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'
 function rakeGarden(garden) {
   return garden.split(' ').map(item => item !== 'gravel' && item !== "rock" ? item = 'gravel' : item).join(' ')
 }
+
+
+/*
+Sort the given array of strings in alphabetical order, case insensitive. For example:
+["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+*/
+
+function sortme(names) {
+  return names.sort( (a,b) => a.localeCompare(b, 'en', {'sensitivity': 'base'}) )
+}
