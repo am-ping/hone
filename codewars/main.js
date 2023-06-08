8ffef7a7cd90a8a2f52b39e7e7dfc30727f58ca8
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-07 20:29:44 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9094,3 +9094,46 @@ Complete the function that takes a string of English-language text and returns t
 function consonantCount(str) {
   return str.replace(/[aeiou\d\W_]/gi, "").length
 }
+
+
+/*
+You are given a string of n lines, each substring being n characters long: For example:
+s = "abcd\nefgh\nijkl\nmnop"
+We will study some transformations of this square of strings.
+- Clock rotation 180 degrees: rot
+rot(s) => "ponm\nlkji\nhgfe\ndcba"
+
+- selfie_and_rot(s) (or selfieAndRot or selfie-and-rot) It is initial string + string obtained by clock rotation 180 degrees with dots interspersed in order (hopefully) to better show the rotation when printed.
+s = "abcd\nefgh\nijkl\nmnop" --> 
+"abcd....\nefgh....\nijkl....\nmnop....\n....ponm\n....lkji\n....hgfe\n....dcba"
+or printed:
+|rotation        |selfie_and_rot
+|abcd --> ponm   |abcd --> abcd....
+|efgh     lkji   |efgh     efgh....
+|ijkl     hgfe   |ijkl     ijkl....   
+|mnop     dcba   |mnop     mnop....
+                           ....ponm
+                           ....lkji
+                           ....hgfe
+                           ....dcba
+Notice that the number of dots is the common length of "abcd", "efgh", "ijkl", "mnop".
+
+Task:
+Write these two functions rotand selfie_and_rot
+and
+high-order function oper(fct, s) where
+fct is the function of one variable f to apply to the string s (fct will be one of rot, selfie_and_rot)
+
+s = "abcd\nefgh\nijkl\nmnop"
+oper(rot, s) => "ponm\nlkji\nhgfe\ndcba"
+oper(selfie_and_rot, s) => "abcd....\nefgh....\nijkl....\nmnop....\n....ponm\n....lkji\n....hgfe\n....dcba"
+*/
+
+let rot = str => str.split("").reverse().join("")
+
+function selfieAndRot(s) {
+  let fHalf = s.replace(/.+/g, t => t + t.replace(/./g, "."))
+  return fHalf + "\n" + rot(fHalf)
+}
+
+let oper = (fct, s) => fct(s)
