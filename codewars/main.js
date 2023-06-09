c4779e2f51689c71bfbfd3c0ab7cfc83df5725cf
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-08 21:58:48 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9137,3 +9137,24 @@ function selfieAndRot(s) {
 }
 
 let oper = (fct, s) => fct(s)
+
+
+/*
+We are going to replicate the kata Base64 Encoding, but instead of creating our own encoding / decoding functions, we will use NodeJS' Buffer module to do it for us.
+
+Create the function String.prototype.toBase64 that encodes a string in Base64. Also create the function String.prototype.fromBase64 that decodes a Base64 string into utf8.
+
+// should return 'dGhpcyBpcyBhIHN0cmluZyEh'
+'this is a string!!'.toBase64(); 
+
+// should return 'this is a string!!'
+'dGhpcyBpcyBhIHN0cmluZyEh'.fromBase64();
+*/
+
+String.prototype.toBase64 = function() {
+  return Buffer.from(this, "utf8").toString("base64")
+}
+
+String.prototype.fromBase64 = function() {
+  return Buffer.from(this, "base64").toString("utf8")
+}
