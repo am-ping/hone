fda14a90a6483382f61bcb932de0488ce0b34d73
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-10 11:59:15 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9170,3 +9170,27 @@ output: "StOp mAkInG SpOnGeBoB MeMeS!"
 function spongeMeme(str) {
   return str.replace(/\w/g, (a,i) => i % 2 ? a.toLowerCase() : a.toUpperCase())
 }
+
+
+/*
+Your job is to implement a function which returns the last D digits of an integer N as a list.
+If D > (the number of digits of N), return all the digits.
+If D <= 0, return an empty list.
+Examples:
+N = 1
+D = 1
+result = [1]
+
+N = 1234
+D = 2
+result = [3, 4]
+
+N = 637547
+D = 6
+result = [6, 3, 7, 5, 4, 7]
+*/
+
+function lastDigit(n, d) {
+  let arr = n.toString().split('').map(item => +item)
+  return d > arr.length ? arr : arr.slice(arr.length-d)
+}
