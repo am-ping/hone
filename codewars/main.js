66af7d7e0b2be921ccb0e17aebb21cdbf5b2e001
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-11 20:17:09 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9194,3 +9194,15 @@ function lastDigit(n, d) {
   let arr = n.toString().split('').map(item => +item)
   return d > arr.length ? arr : arr.slice(arr.length-d)
 }
+
+
+/*
+Complete the function circleArea so that it will return the area of a circle with the given radius. Round the returned number to two decimal places (except for Haskell). If the radius is not positive or not a number, return false.
+circleArea(-1485.86);    //returns false
+circleArea(0);           //returns false
+circleArea(43.2673);     //returns 5881.25
+circleArea(68);          //returns 14526.72
+circleArea("number");    //returns false
+*/
+
+let circleArea = r => r > 0 ? +(Math.PI * r ** 2).toFixed(2) : false
