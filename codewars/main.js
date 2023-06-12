ebfb45d6e8f3c9e0e2f4256537103ed2cdb9520b
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-12 00:59:39 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9206,3 +9206,29 @@ circleArea("number");    //returns false
 */
 
 let circleArea = r => r > 0 ? +(Math.PI * r ** 2).toFixed(2) : false
+
+
+/*
+material1 and material2 cannot be selected at the same time
+material3 and material4 cannot be selected at the same time
+material5 and material6 must be selected at the same time
+material7 or  material8 must be selected(at least one, or both)
+You are given a integer array formula. Array contains only digits 1-8 that represents material 1-8. Your task is to determine if the formula is valid. Returns true if it's valid, false otherwise.
+
+[1,3,7] => true.
+[7,1,2,3] => false.
+[1,3,5,7] => false.
+[1,5,6,7,3] => true.
+[5,6,7] => true.
+[5,6,7,8] => true.
+[6,7,8] => false.
+[7,8] => true.
+*/
+
+function isValid(arr){
+  let a = !(arr.includes(1) && arr.includes(2))
+  let b = !(arr.includes(3) && arr.includes(4))
+  let c = arr.includes(5) === arr.includes(6)
+  let d = arr.includes(7) || arr.includes(8)
+  return a && b && c && d
+}
