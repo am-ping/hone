b996119d1aa465c7db81d9207ff0e33a146c3bab
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-16 02:30:06 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9264,3 +9264,14 @@ Notice that the last element is always included. All numbers will be greater tha
 function solve(arr) {
   return arr.filter((item,i) => arr.slice(i+1).every(n => item > n))
 }
+
+
+/*
+Create a function isDivisible(n,...) that checks if the first argument n is divisible by all other arguments (return true if no other arguments)
+isDivisible(6,1,3)--> true because 6 is divisible by 1 and 3
+isDivisible(12,2)--> true because 12 is divisible by 2
+isDivisible(100,5,4,10,25,20)--> true
+isDivisible(12,7)--> false because 12 is not divisible by 7
+*/
+
+let isDivisible = (n, ...arr) => arr.every(item => n % item == 0)
