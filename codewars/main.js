705cafd36b6e386a5203ae1aee34e50ec9443ca1
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-17 18:40:32 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9275,3 +9275,19 @@ isDivisible(12,7)--> false because 12 is not divisible by 7
 */
 
 let isDivisible = (n, ...arr) => arr.every(item => n % item == 0)
+
+
+/*
+You will be given an array which will include both integers and characters.
+Return an array of length 2 with a[0] representing the mean of the ten integers as a floating point number. There will always be 10 integers and 10 characters. Create a single string with the characters and return it as a[1] while maintaining the original order.
+lst = ['u', '6', 'd', '1', 'i', 'w', '6', 's', 't', '4', 'a', '6', 'g', '1', '2', 'w', '8', 'o', '2', '0']
+Here is an example of your return
+[3.6, "udiwstagwo"]
+*/
+
+function mean(lst){
+  let mean = lst.filter(item => /\d/.test(item)).reduce((a,b) => a + +b, 0) / 10
+  let str = lst.filter(item => /[a-z]/i.test(item)).join('')
+  return [mean, str]
+}
+
