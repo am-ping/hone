25c2ec0d1dc8aae9b67123ea022cd7b185bf848d
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-21 19:03:28 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9345,3 +9345,20 @@ function isVeryEvenNumber(n) {
     return n % 2 == 0
   }
 }
+
+
+/*
+If the integer is divisible by 3, return the string "Java".
+If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+If one of the condition above is true and the integer is even, add "Script" to the end of the string.
+If none of the condition is true, return the string "mocha_missing!"
+1   -->  "mocha_missing!"
+3   -->  "Java"
+6   -->  "JavaScript"
+12  -->  "CoffeeScript"
+*/
+
+function caffeineBuzz(n){
+  let i = n % 3 == 0 && n % 4 == 0 ? 'Coffee' : n % 3 == 0 ? 'Java' : 'mocha_missing!'
+  return (n % 3 == 0 || n % 3 == 0) && n % 2 == 0 ? i + 'Script' : i
+}
