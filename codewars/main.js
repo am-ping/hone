c92c988a3053edd93c8f409688199ea449fd2131
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-24 16:08:05 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9383,3 +9383,20 @@ Return the second largest number in the array.
 function getSecondLargest(nums) {
   return [...new Set(nums)].sort((a,b) => b - a)[1]
 }
+
+
+/*
+Complete the reverseString function; it has one parameter, s. You must perform the following actions:
+Try to reverse string s using the split, reverse, and join methods.
+If an exception is thrown, catch it and print the contents of the exception's message on a new line.
+Print s on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+*/
+
+function reverseString(s) {
+  try {
+      console.log( s.split('').reverse().join('') )
+  } catch(err) {
+      console.log(err.message)
+      console.log(s)
+  }
+}
