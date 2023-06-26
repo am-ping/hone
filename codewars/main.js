59f58e786b900ffa953833732fe198bc62d8e7c7
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-26 17:05:04 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9417,3 +9417,16 @@ function isPositive(a) {
       throw Error('Negative Error')
   }
 }
+
+
+/*
+Complete the function in the editor. It has two parameters: a and b. It must return an object modeling a rectangle that has the following properties:
+length: This value is equal to a
+width: This value is equal to b.
+perimeter: This value is equal to 2 * (a + b)
+area: This value is equal to a * b
+*/
+
+function Rectangle(a, b) {
+  return {length: a, width: b, perimeter: 2 * (a + b), area: a * b}
+}
