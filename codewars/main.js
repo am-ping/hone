bebba50dd02ac4a77175d097868e9e2089553680
arbaaz <mirzaarbaaz123@gmail.com>
2023-06-28 01:00:12 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9442,3 +9442,20 @@ Each of the n subsequent lines contains two space-separated integers describing 
 function getCount(arr) {
   return arr.filter(item => item.x === item.y).length
 }
+
+
+/*
+Create a Polygon class that has the following properties:
+A constructor that takes an array of integer values describing the lengths of the polygon's sides.
+A perimeter() method that returns the polygon's perimeter.
+Locked code in the editor tests the Polygon constructor and the perimeter method.
+*/
+
+class Polygon {
+  constructor(arr) {
+      this.arr = arr
+  }
+  perimeter() {
+      return this.arr.reduce((a,b) => a + b, 0)
+  }
+}
