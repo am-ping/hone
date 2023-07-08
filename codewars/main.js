16b94f990b9f64bf5f0639bc7fc2a24c2071e694
arbaaz <mirzaarbaaz123@gmail.com>
2023-07-08 17:13:58 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9634,3 +9634,51 @@ function allNonConsecutive(arr) {
   arr.forEach((item,index) => index && item !== arr[index - 1] + 1 ? s.push({i: index, n: item}) : item)
   return s
 }
+
+
+/*
+Your task is to take arrays of numbers and compress them into ranges.
+
+The numbers in the array are mostly consecutive. If you convert them as ranges, it will save a lot of space. You should write a function that will convert an array of numbers into a string. A range, or series of consecutive numbers, will be represented as two numbers separated by an underscore, a range of one just by the number its self and multiple ranges separated by commas.
+
+For example,
+The numbers 5, 6, 7, 8 and 9 would be displayed as "5_9"
+The number 6 would just be "6"
+The numbers 3,4,5,6,9 would be "3_6,9"
+
+Using the above system, you should write two functions:
+toRange - convert an array of numbers to a range string
+toArray - convert a range string back to an array
+
+Warnings
+The numbers could arrive all jumbled up so you'll need to sort them Sometimes the same number may appear more than once, duplicates should be discarded.
+
+Edge cases
+An empty array should become an empty string if passed to toRange and vise versa for the toArray function. Also, ranges of 2 digits will take the same space whether they are represented as a sequence or a range. I.e. "5,6,8,9".length === "5_6,8_9".length so there will be no compression, but represent them as a range anyway for consistency.
+*/
+
+// Should return a string representing the ranges
+function toRange(arr) {
+  return [...new Set(arr)].sort((a,b) => a - b)
+    .map((item,i,a) => !i ? item : i && item == a[i - 1] + 1 ? `_${item}` : `,${item}`)
+    .join('')
+    .split(',')
+    .map(item => item.replace(/_.*_/, '_'))
+    .join(',')
+}
+
+// Should return an array
+function toArray(str) {
+  if (str == '') return []
+  let a = str.split(',')
+  let res = []
+  a.map(item => {
+    if (item.includes('_')) {
+      for (let i = +item.slice(0, item.indexOf("_")); i <= +item.slice(item.indexOf("_") + 1); i++) {
+        res.push(i)
+      }
+    }
+    res.push(+item)
+  })
+  return res.filter(item => /[0-9]/.test(item))
+}
