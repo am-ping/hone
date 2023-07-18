fc4db8cc3de586888af424cfe1fb7c545cd0790a
arbaaz <mirzaarbaaz123@gmail.com>
2023-07-18 00:50:38 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9881,3 +9881,20 @@ function passports(filename) {
   
   return arr.filter(item => item.length >= 7).length
 }
+
+
+/* CodeStepByStep
+Write a function named addStars that accepts as a parameter an array of strings, and modifies the array by placing a "*" element between elements, as well as at the start and end of the array.
+
+For example, if the array arr contains ["the", "quick", "brown", "fox"], the call of addStars(arr) should modify it to store ["*", "the", "*", "quick", "*", "brown", "*", "fox", "*"].
+
+If the array passed is empty, you should add a single "*" to the array, resulting in ["*"].
+*/
+
+function addStars(arr) {
+	for(let i = arr.length - 1; i >= 0; i--) {
+        arr.splice(i,0,'*')
+    }
+    arr.push('*')
+    return arr
+}
