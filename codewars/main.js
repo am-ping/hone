1c78bbcc632b7ca4f2bbd65f650cc4e92aa39b9e
arbaaz <mirzaarbaaz123@gmail.com>
2023-07-23 13:22:51 -0400

    coding practice

--- codewars/main.js
+++ codewars/main.js
@@ -9978,3 +9978,12 @@ You may assume that both arrays passed to your function will have lengths of at 
 function contains(a1, a2) {
   return a1.join('').includes(a2.join(''))
 }
+
+
+/* CodeStepByStep
+Write a function named calculateLine which calculates and returns the y value of the line represented with a given x value, m slope, and an intercept b (as seen in the line equation of the form y = m x + b).
+
+For example, a call of calculateLine(5, 2, 4) would return 14 because 14 = 2 * 5 + 4, a call to calculateLine(1, 2, 0) would return 2 because 2 = 2 * 1 + 0, and a call to calculateLine(1, 0.5, 2) would return 2.5 because 2.5 = 0.5 * 1 + 2.
+*/
+
+let calculateLine = (x, m, b) => m * x + b
