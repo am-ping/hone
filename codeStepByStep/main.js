6b6870fee2e0611bd9d07c7e408c6e1ec6dc23bd
arbaaz <mirzaarbaaz123@gmail.com>
2023-08-13 00:35:25 -0400

    coding practice

--- codeStepByStep/main.js
+++ codeStepByStep/main.js
@@ -593,3 +593,26 @@ Write a function named reverse that accepts a string parameter returns a new str
 function reverse(s) {
 	return s.split('').reverse().join('')
 }
+
+
+/*
+Write a function named enoughTimeForLunch that accepts four integers hour1, minute1, hour2, and minute2 as parameters. Each pair of parameters represents a time on the 24-hour clock (for example, 1:36 PM would be represented as 13 and 36). The function should return true if the gap between the two times is long enough to eat lunch: that is, if the second time is at least 45 minutes after the first time. Otherwise the function should return false.
+
+You may assume that all parameter values are valid: the hours are both between 0 and 23, and the minute parameters are between 0 and 59. You may also assume that both times represent times in the same day, e.g. the first time won't represent a time today while the second time represents a time tomorrow. Note that the second time might be earlier than the first time; in such a case, your function should return false.
+
+Here are some example calls to your function and their expected return results:
+
+call	value returned
+enoughTimeForLunch (11, 00, 11, 59)	true
+enoughTimeForLunch (12, 30, 13, 00)	false
+enoughTimeForLunch (12, 30, 13, 15)	true
+enoughTimeForLunch (14, 20, 17, 02)	true
+enoughTimeForLunch (12, 30, 9, 30)	false
+enoughTimeForLunch (12, 00, 11, 55)	false
+*/
+
+function enoughTimeForLunch(h1, m1, h2, m2) {
+	let mins1 = (h1 > 0) ? (h1 * 60) + m1 : 0
+    let mins2 = (h2 > 0) ? (h2 * 60) + m2 : 0
+    return mins2 - mins1 >= 45
+}
