28efd99703e4de720960e570f80b3f5d29fa8bb1
arbaaz <mirzaarbaaz123@gmail.com>
2023-08-16 20:26:32 -0400

    coding practice

--- codeStepByStep/main.js
+++ codeStepByStep/main.js
@@ -663,3 +663,16 @@ function sumOfRange(min, max) {
     }
     return s
 }
+
+
+/*
+Write a function named sumUpTo that accepts an integer parameter n and returns the sum of the integers from 1 through n inclusive. For example, sumUpTo(5) returns 15, since 1 + 2 + 3 + 4 + 5 = 15. You may assume that the value of n is at least 1.
+*/
+
+function sumUpTo(n) {
+	let r = 0
+    for (let i = 1; i <= n; i++) {
+        r += i
+    }
+    return r
+}
