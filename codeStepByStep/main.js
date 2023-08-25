83f073a53ab91c8e56bc787e74251e0c5f9b440b
arbaaz <mirzaarbaaz123@gmail.com>
2023-08-24 20:01:56 -0400

    coding practice

--- codeStepByStep/main.js
+++ codeStepByStep/main.js
@@ -783,3 +783,12 @@ function boxOfStars(w, h) {
         }
     }
 }
+
+
+/*
+Write a function named circleArea that accepts the radius of a circle as a parameter (as a real number) and returns the area of a circle with that radius. For example, the call of area(2.0) should return 12.566370614359172. You may assume that the radius passed is a non-negative number.
+*/
+
+function circleArea(r) {
+	return Math.PI * r ** 2
+}
