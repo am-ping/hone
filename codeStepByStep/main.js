f0e36d8e97858ad500ad33bfa57d6bb0eef9bf5d
arbaaz <mirzaarbaaz123@gmail.com>
2023-08-29 19:34:34 -0400

    coding practice

--- codeStepByStep/main.js
+++ codeStepByStep/main.js
@@ -834,3 +834,18 @@ function factorCount(n) {
     }
     return arr.length
 }
+
+
+/*
+Write a function named factorial that accepts an integer n as a parameter and returns the factorial of n, or n!.
+
+A factorial of an integer is defined as the product of all integers from 1 through that integer inclusive. For example, the call of factorial(4) should return 1 * 2 * 3 * 4, or 24. The factorial of 0 and 1 are defined to be 1. You may assume that the value passed is non-negative and that its factorial can fit in the range of type number.
+*/
+
+function factorial(n) {
+    let arr = []
+	for (let i = 1; i <= n; i++) {
+        arr.push(i)
+    }
+    return arr.reduce((a,b) => a * b, 1)
+}
