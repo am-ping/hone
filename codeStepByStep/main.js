67d2d20b8c24d9ca27722edc09bd1b4065d3ef83
arbaaz <mirzaarbaaz123@gmail.com>
2023-08-30 19:36:28 -0400

    coding practice

--- codeStepByStep/main.js
+++ codeStepByStep/main.js
@@ -849,3 +849,16 @@ function factorial(n) {
     }
     return arr.reduce((a,b) => a * b, 1)
 }
+
+
+/*
+In physics, a common useful equation for finding the position s of a body in linear motion at a given time t (in seconds), based on its initial position s0, initial velocity v0, and rate of acceleration a, is the following:
+
+s = s0 + v0 t + 1/2 * at2
+
+Write a function getDisplacement that takes in parameters s0, t, and v0 and returns the resulting position, s, using 9.8 as the acceleration constant for gravity.
+*/
+
+function getDisplacement(s0, v0, t) {
+	return s0 + v0 * t + 1/2 * 9.8 * t ** 2
+}
