43e812a423644ecdcfeba5098af0b852e8dc3339
arbaaz <mirzaarbaaz123@gmail.com>
2023-09-03 19:29:08 -0400

    coding practice

--- codeStepByStep/main.js
+++ codeStepByStep/main.js
@@ -883,3 +883,12 @@ function getLastDigit(n) {
     let l = Math.abs(n).toPrecision().length
 	return +Math.abs(n).toPrecision()[l -1]
 }
+
+
+/*
+Write a function isMultiple that accepts two non-negative number parameters a and b, and returns true if a is a multiple of b, and false otherwise. For example, the call of isMultiple(15, 5) would return true because 15 = 5 * 3. You may assume that a and b are non-negative integers and that b is not 0.
+*/
+
+function isMultiple(a, b) {
+	return Number.isSafeInteger(a / b)
+}
