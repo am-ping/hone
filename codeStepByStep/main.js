25c13636e738913cf8c65533f04aea4839780558
arbaaz <mirzaarbaaz123@gmail.com>
2023-09-14 23:38:04 -0400

    coding practice

--- codeStepByStep/main.js
+++ codeStepByStep/main.js
@@ -1140,3 +1140,14 @@ function isKeithNumber(n) {
     }
     return arr[arr.length - 1] === n
 }
+
+
+/*
+Write a function named isMagicSquare that accepts a two-dimensional array of integers as a parameter and returns true if it is a magic square. A square matrix is a magic square if it is square in shape (same number of rows as columns, and every row the same length), and all of its row, column, and diagonal sums are equal. For example, [[2, 7, 6], [9, 5, 1], [4, 3, 8]] is a magic square because all eight of the sums are exactly 15.
+*/
+
+function isMagicSquare(a) {
+    if (a.length == 0) return true
+    let s = a[0].reduce((a,b) => a + b, 0)
+	return a.every(item => item.reduce((a,b) => a + b, 0) == s && item.length === a.length)    
+}
