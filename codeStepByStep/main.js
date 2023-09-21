fa887194c0a1f880d0f34b5b6ce4fc070d5defe2
arbaaz <mirzaarbaaz123@gmail.com>
2023-09-21 01:07:07 -0400

    coding practice

--- codeStepByStep/main.js
+++ codeStepByStep/main.js
@@ -1231,3 +1231,12 @@ function print(data) {
         console.log(`element [${i}] is ${data[i]}`)
     }
 }
+
+
+/*
+Write a function named removeAll that accepts an array of Strings and a single String as parameters, and returns a new array. The returned array should be equivalent to the parameter array but with all occurrences of the String parameter removed, ignoring case. The array parameter should remain unchanged as a result of the function call. For example, if an array named words contains ["foo", "bar", "baz", "Foo", "FOO"], the call of removeAll(words, "foo") should return ["bar", "baz"].
+*/
+
+function removeAll(words, word) {
+	return words.filter(item => item.toLowerCase() !== word.toLowerCase())
+}
