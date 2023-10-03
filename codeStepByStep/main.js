b4f364e29854adde51158ff0bee47f9574be92ca
am-ping <63935001+am-ping@users.noreply.github.com>
2023-10-02 21:43:23 -0400

    coding practice

--- codeStepByStep/main.js
+++ codeStepByStep/main.js
@@ -1360,3 +1360,24 @@ let squares = []
 for (let i = 0; i < 11; i++) {
     squares.push(i * i)
 }
+
+
+/*
+Write a function named switchPairs that switches the order of values in an array in a pairwise fashion. Your function should accept an array as a parameter and should switch the order of the first two values, then switch the order of the next two, switch the order of the next two, and so on. If there are an odd number of values in the list, the final element is not moved.
+
+For example, if the array initially stores [1, 4, 8, -3, 2, 7, 0], then your function should switch the first pair, (1 and 4), the second pair (8 and -3) and the third pair (2 and 7) to yield [4, 1, -3, 8, 7, 2, 0]. The behavior should be the same for valid arrays of any type. For example, if an array initially stores [0, "foo", -1, "bar", "baz"], the call of switchPairs should change the array's ordering to ["foo", 0, "bar", -1, "baz"].
+*/
+
+function switchPairs(list) {
+    let temp;
+	for (let i = 0; i < list.length; i++) {
+        if (i % 2 == 0) {
+            temp = list[i];
+            if (i !== list.length-1) {
+                list[i] = list[i+1];
+            }
+        } else {
+            list[i] = temp;
+        }
+    }
+}
